d78e0282f0c0b9f424e6a14f15b4527c074d1902
Theo Kalifa <theo.kalifa@free.fr>
2019-10-22 23:29:58 +0200

    Ajout de la rotation

--- src/fr/fjdhj/PacMan/gameLogic/Direction.java
+++ src/fr/fjdhj/PacMan/gameLogic/Direction.java
@@ -1,19 +1,25 @@
 package fr.fjdhj.PacMan.gameLogic;
 
 public enum Direction{
-    UP    ("up"), 
-    DOWN  ("down"), 
-    LEFT  ("left"), 
-    RIGHT ("right");
+    UP    ("up", 90), 
+    DOWN  ("down", 270), 
+    LEFT  ("left", 180), 
+    RIGHT ("right", 0);
 	
 	private final String direction;
+	private final int rotate;
 	
-	Direction(String direction){
+	Direction(String direction, int rotate){
 		this.direction = direction;
+		this.rotate = rotate;
 	}
 	
 	public String toString() {
 		return direction;
+	}
+	
+	public int getRotate() {
+		return rotate;
 	}
 }
 
